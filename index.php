85a457cf0fa1acc0f13263afc707b38c07850284
juniorbdb <junior_escjr@yahoo.com.br>
2015-09-16 00:00:02 -0300

    inicio do offset

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Grid</title>
+        <title>Grid - 12 colunas</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="stylesheet" href="css/reset.css">
         <link rel="stylesheet" href="css/grid-12-cols.css">
@@ -88,6 +90,45 @@
                     <div class="col col6">
                         <div class="box-custom">
                             .col6
+                        </div>
+                    </div>
+                <?php endfor; ?>
+            </div>
+            <!-- /row -->
+
+
+            <h1 class="titulo">GRID COM OFFSET</h1>
+
+            <!-- row -->
+            <div class="row">
+                <?php for ($i=0; $i < 6; $i++) : ?>
+                    <div class="col col1 offset1">
+                        <div class="box-custom">
+                            .offset1
+                        </div>
+                    </div>
+                <?php endfor; ?>
+            </div>
+            <!-- /row -->
+
+            <!-- row -->
+            <div class="row">
+                <?php for ($i=0; $i < 3; $i++) : ?>
+                    <div class="col col2 offset2">
+                        <div class="box-custom">
+                            .offset2
+                        </div>
+                    </div>
+                <?php endfor; ?>
+            </div>
+            <!-- /row -->
+
+            <!-- row -->
+            <div class="row">
+                <?php for ($i=0; $i < 2; $i++) : ?>
+                    <div class="col col3 offset3">
+                        <div class="box-custom">
+                            .offset3
                         </div>
                     </div>
                 <?php endfor; ?>
