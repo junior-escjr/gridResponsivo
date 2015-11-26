418947ec69c11ca4411b35da01ae2ed15860d1b9
juniorbdb <junior_escjr@yahoo.com.br>
2015-11-25 23:07:13 -0200

    ajuste no grid - troquei o float pelo display: inline-block, ajuste na margen do offset e a criação para cada breakpoint

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <link rel="stylesheet" href="css/reset.css">
         <link rel="stylesheet" href="css/grid-12-cols.css">
         <link rel="stylesheet" href="css/style.css">
-        
+
         <!--[if lt IE 9]>
         <script src="js/ie/html5shiv.min.js"></script>
         <script src="js/ie/selectivizr-min.js"></script>
@@ -22,189 +22,194 @@
         <!-- wrapper -->
         <div class="wrapper">
 
+            <!-- container -->
+            <div class="container">
 
-            <div class="row">
-                <div class="col-xs-12 col-sm-4 col-md-6 col-lg-4">
-                    <div class="box-custom">.col1</div>
+                <div class="row">
+                    <div class="col-sm-4 col-md-6 col-lg-4">
+                        <div class="box-custom">.col1</div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-md-6 col-lg-4">
+                        <div class="box-custom">.col1</div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-md-12 col-lg-4">
+                        <div class="box-custom">.col1</div>
+                    </div>
                 </div>
-                <div class="col-xs-12 col-sm-4 col-md-6 col-lg-4">
-                    <div class="box-custom">.col1</div>
-                </div>
-                <div class="col-xs-12 col-sm-4 col-md-12 col-lg-4">
-                    <div class="box-custom">.col1</div>
-                </div>
-            </div>
 
 
-            <!-- row -->
-            <div class="row">
-                <?php for ($i=0; $i < 12; $i++) : ?>
-                    <div class="col-lg-1">
+                <!-- row -->
+                <div class="row">
+                    <?php for ($i=0; $i < 12; $i++) : ?>
+                        <div class="col-lg-1">
+                            <div class="box-custom">
+                                .col1
+                            </div>
+                        </div>
+                    <?php endfor; ?>
+                </div>
+                <!-- /row -->
+
+
+                <!-- row -->
+                <div class="row">
+                    <?php for ($i=0; $i < 6; $i++) : ?>
+                        <div class="col-lg-2">
+                            <div class="box-custom">
+                                .col2
+                            </div>
+                        </div>
+                    <?php endfor; ?>
+                </div>
+                <!-- /row -->
+
+                <!-- row -->
+                <div class="row">
+                    <?php for ($i=0; $i < 4; $i++) : ?>
+                        <div class="col-lg-3">
+                            <div class="box-custom">
+                                .col3
+                            </div>
+                        </div>
+                    <?php endfor; ?>
+                </div>
+                <!-- /row -->
+
+                <!-- row -->
+                <div class="row">
+                    <?php for ($i=0; $i < 3; $i++) : ?>
+                        <div class="col-md-4">
+                            <div class="box-custom">
+                                .col4
+                            </div>
+                        </div>
+                    <?php endfor; ?>
+                </div>
+                <!-- /row -->
+
+                <!-- row -->
+                <div class="row">
+                    <div class="col-sm-5">
                         <div class="box-custom">
-                            .col1
+                            .col5
                         </div>
                     </div>
-                <?php endfor; ?>
-            </div>
-            <!-- /row -->
+                    <div class="col-sm-5">
+                        <div class="box-custom">
+                            .col5
+                        </div>
+                    </div>
+                    <div class="col-sm-2">
+                        <div class="box-custom">
+                            .col2
+                        </div>
+                    </div>
+                </div>
+                <!-- /row -->
+
+                <!-- row -->
+                <div class="row">
+                    <?php for ($i=0; $i < 2; $i++) : ?>
+                        <div class="col-xs-6">
+                            <div class="box-custom">
+                                .col6
+                            </div>
+                        </div>
+                    <?php endfor; ?>
+                </div>
+                <!-- /row -->
+
+                <!-- row -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="box-custom">
+                            .col12
+                        </div>
+                    </div>
+                </div>
+                <!-- /row -->
 
 
-            <!-- row -->
-            <div class="row">
-                <?php for ($i=0; $i < 6; $i++) : ?>
+                <h1 class="titulo">GRID COM OFFSET</h1>
+
+                <!-- row -->
+                <div class="row">
+                    <?php for ($i=0; $i < 6; $i++) : ?>
+                        <div class="col-lg-1 offset1">
+                            <div class="box-custom">
+                                .offset1
+                            </div>
+                        </div>
+                    <?php endfor; ?>
+                </div>
+                <!-- /row -->
+
+                <!-- row -->
+                <div class="row">
+                    <?php for ($i=0; $i < 3; $i++) : ?>
+                        <div class="col-lg-2 offset2">
+                            <div class="box-custom">
+                                .offset2
+                            </div>
+                        </div>
+                    <?php endfor; ?>
+                </div>
+                <!-- /row -->
+
+                <!-- row -->
+                <div class="row">
+                    <?php for ($i=0; $i < 2; $i++) : ?>
+                        <div class="col-lg-3 offset3">
+                            <div class="box-custom">
+                                .offset3
+                            </div>
+                        </div>
+                    <?php endfor; ?>
+                </div>
+                <!-- /row -->
+
+                <!-- row -->
+                <div class="row">
+                    <div class="col-lg-4 offset4">
+                        <div class="box-custom">
+                            .offset4
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="box-custom">
+                            .col4
+                        </div>
+                    </div>
+                </div>
+                <!-- /row -->
+
+                <!-- row -->
+                <div class="row">
+                    <div class="col-lg-5 offset5">
+                        <div class="box-custom">
+                            .offset5
+                        </div>
+                    </div>
                     <div class="col-lg-2">
                         <div class="box-custom">
                             .col2
                         </div>
                     </div>
-                <?php endfor; ?>
-            </div>
-            <!-- /row -->
+                </div>
+                <!-- /row -->
 
-            <!-- row -->
-            <div class="row">
-                <?php for ($i=0; $i < 4; $i++) : ?>
-                    <div class="col-lg-3">
+                <!-- row -->
+                <div class="row">
+                    <div class="col-lg-6 offset6">
                         <div class="box-custom">
-                            .col3
+                            .offset6
                         </div>
                     </div>
-                <?php endfor; ?>
-            </div>
-            <!-- /row -->
-
-            <!-- row -->
-            <div class="row">
-                <?php for ($i=0; $i < 3; $i++) : ?>
-                    <div class="col-md-4">
-                        <div class="box-custom">
-                            .col4
-                        </div>
-                    </div>
-                <?php endfor; ?>
-            </div>
-            <!-- /row -->
-
-            <!-- row -->
-            <div class="row">
-                <div class="col-sm-5">
-                    <div class="box-custom">
-                        .col5
-                    </div>
                 </div>
-                <div class="col-sm-5">
-                    <div class="box-custom">
-                        .col5
-                    </div>
-                </div>
-                <div class="col-sm-2">
-                    <div class="box-custom">
-                        .col2
-                    </div>
-                </div>
+                <!-- /row -->
+
             </div>
-            <!-- /row -->
-
-            <!-- row -->
-            <div class="row">
-                <?php for ($i=0; $i < 2; $i++) : ?>
-                    <div class="col-xs-6">
-                        <div class="box-custom">
-                            .col6
-                        </div>
-                    </div>
-                <?php endfor; ?>
-            </div>
-            <!-- /row -->
-
-            <!-- row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="box-custom">
-                        .col12
-                    </div>
-                </div>
-            </div>
-            <!-- /row -->
-
-
-            <h1 class="titulo">GRID COM OFFSET</h1>
-
-            <!-- row -->
-            <div class="row">
-                <?php for ($i=0; $i < 6; $i++) : ?>
-                    <div class="col-lg-1 offset1">
-                        <div class="box-custom">
-                            .offset1
-                        </div>
-                    </div>
-                <?php endfor; ?>
-            </div>
-            <!-- /row -->
-
-            <!-- row -->
-            <div class="row">
-                <?php for ($i=0; $i < 3; $i++) : ?>
-                    <div class="col-lg-2 offset2">
-                        <div class="box-custom">
-                            .offset2
-                        </div>
-                    </div>
-                <?php endfor; ?>
-            </div>
-            <!-- /row -->
-
-            <!-- row -->
-            <div class="row">
-                <?php for ($i=0; $i < 2; $i++) : ?>
-                    <div class="col-lg-3 offset3">
-                        <div class="box-custom">
-                            .offset3
-                        </div>
-                    </div>
-                <?php endfor; ?>
-            </div>
-            <!-- /row -->
-
-            <!-- row -->
-            <div class="row">
-                <div class="col-lg-4 offset4">
-                    <div class="box-custom">
-                        .offset4
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="box-custom">
-                        .col4
-                    </div>
-                </div>
-            </div>
-            <!-- /row -->
-
-            <!-- row -->
-            <div class="row">
-                <div class="col-lg-5 offset5">
-                    <div class="box-custom">
-                        .offset5
-                    </div>
-                </div>
-                <div class="col-lg-2">
-                    <div class="box-custom">
-                        .col2
-                    </div>
-                </div>
-            </div>
-            <!-- /row -->
-
-            <!-- row -->
-            <div class="row">
-                <div class="col-lg-6 offset6">
-                    <div class="box-custom">
-                        .offset6
-                    </div>
-                </div>
-            </div>
-            <!-- /row -->
+            <!-- /container -->
 
         </div>
         <!-- /wrapper -->
